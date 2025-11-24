bee3b7346943e380afbe111eab98a9f4052fec88
leece03 <164038699+leece03@users.noreply.github.com>
2025-11-25 03:22:32 +0900

    chore: 날짜 수정

--- app/src/main/java/com/example/re0/di/DatabaseModule.kt
+++ app/src/main/java/com/example/re0/di/DatabaseModule.kt
@@ -48,61 +48,61 @@ object DatabaseModule {
                                 Achievement(
                                     title = "재활용마스터",
                                     iconUrl = R.drawable.rectangle1_2,
-                                    date = "2025.10.10",
+                                    date = "2025.11.21",
                                     isDone = false
                                 ),
                                 Achievement(
                                     title = "분리수거천재",
                                     iconUrl = R.drawable.rectangle1_1,
-                                    date = "2025.10.10",
+                                    date = "2025.11.22",
                                     isDone = false
                                 ),
                                 Achievement(
                                     title = "텀블러킹",
                                     iconUrl = R.drawable.rectangle1_2,
-                                    date = "2025.10.08",
+                                    date = "2025.11.24",
                                     isDone = false
                                 ),
                                 Achievement(
                                     title = "분리수거천재",
                                     iconUrl = R.drawable.rectangle1_1,
-                                    date = "2025.10.06",
+                                    date = "2025.11.24",
                                     isDone = false
                                 ),
                                 Achievement(
                                     title = "텀블러킹",
                                     iconUrl = R.drawable.rectangle1_2,
-                                    date = "2025.10.10",
+                                    date = "2025.11.20",
                                     isDone = false
                                 ),
                                 Achievement(
                                     title = "재활용마스터",
                                     iconUrl = R.drawable.rectangle1_1,
-                                    date = "2025.10.07",
+                                    date = "2025.11.19",
                                     isDone = false
                                 ),
                                 Achievement(
                                     title = "재활용마스터",
                                     iconUrl = R.drawable.rectangle1_2,
-                                    date = "2025.10.07",
+                                    date = "2025.11.19",
                                     isDone = false
                                 ),
                                 Achievement(
                                     title = "분리수거천재",
                                     iconUrl = R.drawable.rectangle1_1,
-                                    date = "2025.10.07",
+                                    date = "2025.11.20",
                                     isDone = false
                                 ),
                                 Achievement(
                                     title = "분리수거천재",
                                     iconUrl = R.drawable.rectangle1_1,
-                                    date = "2025.10.07",
+                                    date = "2025.11.20",
                                     isDone = false
                                 ),
                                 Achievement(
                                     title = "분리수거천재",
                                     iconUrl = R.drawable.rectangle1_1,
-                                    date = "2025.10.04",
+                                    date = "2025.11.20",
                                     isDone = false
                                 )
                             )
