ac6b89952b3a3298bb0a652539d6ddde6cf20652
leece03 <164038699+leece03@users.noreply.github.com>
2025-11-25 14:37:53 +0900

    style: Mint 색상 코드 변경

--- app/src/main/java/com/example/re0/ui/theme/Color.kt
+++ app/src/main/java/com/example/re0/ui/theme/Color.kt
@@ -10,7 +10,7 @@ val Purple40 = Color(0xFF6650a4)
 val PurpleGrey40 = Color(0xFF625b71)
 val Pink40 = Color(0xFF7D5260)
 
-val Mint: Color = Color(0xFF01E7C5)
+val Mint: Color = Color(0xFF65F9E3)
 
 val SkyBlue: Color = Color(color =  0xFFE3F3FB)
 
